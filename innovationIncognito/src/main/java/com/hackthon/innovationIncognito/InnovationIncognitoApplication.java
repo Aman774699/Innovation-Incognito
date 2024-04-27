5851e346a65c124eb15061e60c21ee48bb6e64ac
Aman Vishwakarma <104434057+Aman774699@users.noreply.github.com>
2024-04-27 20:22:22 +0530

    Add files via upload

---
+++ innovationIncognito/src/main/java/com/hackthon/innovationIncognito/InnovationIncognitoApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon.innovationIncognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationIncognitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationIncognitoApplication.class, args);
+	}
+
+}
---
+++ innovationIncognito/src/test/java/com/hackthon/innovationIncognito/InnovationIncognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthon.innovationIncognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationIncognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
