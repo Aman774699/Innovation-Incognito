d9010a434bfa33c7c1aebde93557d1596554b7e8
Aman774699 <amanvishwakarma922@gmail.com>
2024-04-27 20:29:25 +0530

    Aman vishwakarma

--- innovationIncognito/src/main/java/com/hackthon/innovationIncognito/InnovationIncognitoApplication.java
+++ innovationIncognito/src/main/java/com/hackthon/innovationIncognito/InnovationIncognitoApplication.java
@@ -8,6 +8,8 @@ public class InnovationIncognitoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnovationIncognitoApplication.class, args);
+		System.out.println();
+
 	}
 
 }
